1b82f456a6052365e5d79aa1894d4ff47f1b2325
Laura Huckins <laura.huckins@mssm.edu>
2016-09-08 15:34:40 -0400

    Update vcf_to_gen.sh

--- vcf_to_gen.sh
+++ vcf_to_gen.sh
@@ -5,7 +5,7 @@ do
 
     cat Amanda_vcfids | tr ':' ' ' | awk -v c=$chr '$1==c {print $1":"$2}' > Amanda_vcfids.$chr # Get ids in format for qctool
 
-    qctool -g chr21.vcf.gz -og Amanda.21.gen -incl-rsids Amanda_vcfids.21
+    qctool -g chr$chr.vcf.gz -og Amanda.$chr.gen -incl-rsids Amanda_vcfids.$chr
 
     cat CMC_eqtl_SVA_Caucasian_combined+BP_chrS_cis_conditional.out.bp | awk -v c=$chr '$1==c {print $1":"$3, $2}' > Amanda.mapping.$chr # get mapping file to upate chr:pos to rsids
 
@@ -16,9 +16,9 @@ do
         old=$(echo $line | awk '{print $1}')
         new=$(echo $line | awk '{print $2}')
 
-        sed -i "s/$old/$new/1" Amanda.21.gen
+        sed -i "s/$old/$new/1" Amanda.$chr.gen
 
-    done<Amanda.mapping.21
+    done<Amanda.mapping.$chr
 
 done
 
